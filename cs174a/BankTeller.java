7013e107ae3680ed858bc98c2be86508801fc5cb
xueyiyao <alleny111@gmail.com>
2019-12-03 19:52:39 -0800

    Added writeCheck, generateMonthlyReport, generateDTER and fixed minor bugs

--- cs174a/BankTeller.java
+++ cs174a/BankTeller.java
@@ -32,16 +32,29 @@ public class BankTeller {
             String tax_id = "";
             switch(choice) {
                 case "0": //Enter Check Transaction
-                    //writecheck(aid, amount)
+                    System.out.println("Enter the account that the check is written from:");
+                    String aid_check = s.nextLine();
+                    System.out.println("Enter the amount that the check is written for:");
+                    String strAmount = s.nextLine();
+                    while(checkAmountInput(strAmount) == false){
+                        System.out.println("Not a valid amount. Please re-enter:");
+                        strAmount = s.nextLine();
+                    }
+                    String response = app.writeCheck(aid_check, Double.parseDouble(strAmount));
+                    if(response.charAt(0) == '0'){
+                        System.out.println("\nCheck succesfully written!");
+                    }
                     break;
-                case "1": //Generate Monthly Statement
-                    //generateMontly()
+                case "1": //Generate Monthly Statement\
+                    System.out.println("Enter the tax id that the monthly statement should be generated for:");
+                    tax_id = s.nextLine();
+                    app.generateMonthly(tax_id);
                     break;
                 case "2": //List Closed Accounts 
                     app.listClosedAccounts();
                     break;  
-                case "3": //Generate Government Drug and Tax Evasion Report
-                    //generateDTER()
+                case "3": //Generate Government Drug and Tax Evasion Report (Generate appropriate output if no deposits > 10000)
+                    app.generateDTER();
                     break;
                 case "4": //Generate Customer Report 
                     System.out.println("Enter the tax ID you would like to generate a report for: ");
@@ -49,9 +62,9 @@ public class BankTeller {
                     app.generateCustomerReport(tin);
                     break;
                 case "5": //Add Interest
-                    //addInterest()
+                    //app.addInterest()
                     break;
-                case "6": //Create Account (Problems: handle invalid option inputs)
+                case "6": //Create Account
                     System.out.println("\nCreating account for:\n0: Returning Customer\n1: New Customer");
                     String createCus = s.nextLine();
                     while(createCus.equals("0") == false && createCus.equals("1") == false){
@@ -64,6 +77,10 @@ public class BankTeller {
                         System.out.println("\nWhat type of account would you like to create?");
                         System.out.println("0: Checking (Interest)\n1: Checking (Student)\n2: Savings\n3: Pocket\n4: Cancel");
                         createAcc = s.nextLine();
+                        while(checkIntegerInput(createAcc) == false){
+                            System.out.println("Not a valid amount. Please re-enter:");
+                            createAcc = s.nextLine();
+                        }
                         while(Integer.parseInt(createAcc) > 4 || Integer.parseInt(createAcc) < 0){
                             System.out.println("Not a valid Option. Please try again.");
                             System.out.println("0: Checking (Interest)\n1: Checking (Student)\n2: Savings\n3: Pocket\n4: Cancel");
@@ -119,6 +136,10 @@ public class BankTeller {
                         System.out.println("\nWhat type of account would you like to create?");
                         System.out.println("0: Checking (Interest)\n1: Checking (Student)\n2: Savings\n3: Cancel");
                         createAcc = s.nextLine();
+                        while(checkIntegerInput(createAcc) == false){
+                            System.out.println("Not a valid amount. Please re-enter:");
+                            createAcc = s.nextLine();
+                        }
                         while(Integer.parseInt(createAcc) > 3 || Integer.parseInt(createAcc) < 0){
                             System.out.println("Not a valid Option. Please try again.");
                             System.out.println("0: Checking (Interest)\n1: Checking (Student)\n2: Savings\n3: Cancel");
@@ -150,7 +171,7 @@ public class BankTeller {
                         }
                     }
                     break;
-                case "7": //Delete Closed Accounts and Customers (Problems: bug, check below; on complete, generate success message)
+                case "7": //Delete Closed Accounts and Customers (Problems: bug, check below; on complete, generate success message, ORA if no closed)
                     app.deleteClosed(); 
                     break;
                     //BUG: SOMETIMES DELETES CUSTOMERS/OWNERS EVEN IF AN ACCOUNT IS STILL ACTIVE:
@@ -170,6 +191,35 @@ public class BankTeller {
                     System.out.println("Not a valid transaction number.");
                     break;
             }
+        }
+    }
+
+        //check to see if inputted amount is valid
+    public boolean checkAmountInput(String strAmount){
+        try { 
+            double d = Double.parseDouble(strAmount); 
+            if(d < 0){
+                return false;
+            }
+            return true;
+        }
+        catch(Exception e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean checkIntegerInput(String input){
+        try { 
+            int i = Integer.parseInt(input); 
+            if(i < 0){
+                return false;
+            }
+            return true;
+        }
+        catch(Exception e) {
+            System.err.println(e.getMessage());
+            return false;
         }
     }
 }
